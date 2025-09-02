4559512868ee4a068a148757a25b481147d63523
raj-softhealer <raj@softhealer.com>
2025-09-02 18:41:55 +0530

    Delete sh_library_management directory

--- sh_library_management/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import book_inventory_model
-from . import author_details
-from . import category_model
--- sh_library_management/models/category_model.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields,models
-
-
-class Categories(models.Model):
-    _name="book.categories"
-    _description="Book Categories"
-
-    name=fields.Char(string="Category")
