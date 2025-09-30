57e5a4a0ec87ee7d2da7c9d1e0844ec2ab4f0cd4
raj-softhealer <raj@softhealer.com>
2025-09-30 12:56:56 +0530

    Add files via upload

--- sh_sale_alternative_products/models/__init__.py
+++ sh_sale_alternative_products/models/__init__.py
@@ -1,4 +1,2 @@
-from . import sh_alternative_product
-from . import sh_action_open_wizard
-from . import sh_add_alternate_prouct_wizard_model
-from . import sh_stock_view_wizard
+from . import sh_alternative_product_model
+from . import sh_action_open_wizard_model
