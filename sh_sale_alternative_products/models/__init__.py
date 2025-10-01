e6cc7bff6aef45bed2133f253fde97198afc4820
raj-softhealer <raj@softhealer.com>
2025-10-01 10:01:12 +0530

    Delete sh_sale_alternative_products directory

--- sh_sale_alternative_products/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import sh_alternative_product_model
-from . import sh_action_open_wizard_model
--- sh_sale_alternative_products/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import sh_add_alternate_prouct_wizard_model
-from . import sh_stock_view_wizard
-
